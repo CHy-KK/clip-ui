4a8a70ecbd5dabdbeb08d0aab87c01ed8a724112
CHy-KK <ychanjusuki@gmail.com>
2024-03-16 19:02:35 +0800

    select logic

--- assets/Script/Controller.ts
+++ assets/Script/Controller.ts
@@ -9,6 +9,7 @@ type DataPoint = {
     value: number;
     idx: number;    // 映射到原数组中的序号
 };
+
 type rectSize = {
     left: number,
     right: number, 
@@ -16,11 +17,21 @@ type rectSize = {
     top: number
 };
 
+enum SelectingType {
+    None = 0,
+    One = 1,
+    Range = 2,
+    Multi = 3
+}
+
 @ccclass('test')
 export class test extends Component {
 
     @property(Node)
-    public graphics: Node = null;
+    public ScatterGraphic: Node = null;
+
+    @property(Node)
+    public SelectGraphic: Node = null;
 
     @property(Node)
     public UICanvas: Node = null;
@@ -28,37 +39,44 @@ export class test extends Component {
     @property(Prefab)
     public SelectNode: Prefab = null;
 
+
     private data: Vec3[] = [];
     private pointTree: DataPoint[][][] = [  ];
     private canvasSize: Vec2 = new Vec2(0);
-    private graph: Graphics;
+    private scatterGraph: Graphics;
+    private selectGraph: Graphics;
     private scatterRange: rectSize; // x-min, x-max, y-min, y-max
     private scatterWidth: number;
     private scatterHeight: number;
     private isShow: boolean = true;
     private selectNodeList: Node[] = [];
-    private selectPanelPos: rectSize;
+    private quadPanelPos: rectSize;
 
     // 交互数据
     private selectPos: Vec2 = new Vec2(0);
+    private selectMovingPos: Vec2 =  new Vec2(0);
     private isMove: boolean = false;
     private isSelect: boolean = false;
-    private isSelectingOne: boolean = false;
-    private isSelectingAny: boolean = false;
+    private isSelectCtrl: boolean = false;
+    private selectType: SelectingType = SelectingType.None;
+    
+    // private isSelectingOne: boolean = false;
+    // private isSelectingRange: boolean = false;
+    // private isSelectingMulti: boolean = false;
 
     start() {
         // Initialize
         this.canvasSize.x = this.UICanvas.getComponent(UITransform).contentSize.x;
         this.canvasSize.y = this.UICanvas.getComponent(UITransform).contentSize.y;
-        const selectPanel = this.UICanvas.getChildByName('BackUI').getChildByName('selectPanel');
-        this.selectPanelPos = {
-            right: this.canvasSize.x - selectPanel.getComponent(Widget).right,
-            top: this.canvasSize.y - selectPanel.getComponent(Widget).top,
+        const quadPanel = this.UICanvas.getChildByName('BackUI').getChildByName('quadPanel');
+        this.quadPanelPos = {
+            right: this.canvasSize.x - quadPanel.getComponent(Widget).right,
+            top: this.canvasSize.y - quadPanel.getComponent(Widget).top,
             left: 0,
             bottom: 0
         };
-        this.selectPanelPos.left = this.selectPanelPos.right - selectPanel.getComponent(UITransform).contentSize.x;
-        this.selectPanelPos.bottom = this.selectPanelPos.top - selectPanel.getComponent(UITransform).contentSize.y;
+        this.quadPanelPos.left = this.quadPanelPos.right - quadPanel.getComponent(UITransform).contentSize.x;
+        this.quadPanelPos.bottom = this.quadPanelPos.top - quadPanel.getComponent(UITransform).contentSize.y;
 
 
         let xhr = new XMLHttpRequest();
@@ -96,13 +114,20 @@ export class test extends Component {
             this.scatterRange.top = Math.max(this.scatterRange.top, value.y);
         })
 
-        this.graph = this.graphics.getComponent(Graphics);
+        this.scatterGraph = this.ScatterGraphic.getComponent(Graphics);
+        this.selectGraph = this.SelectGraphic.getComponent(Graphics);
+        console.log(this.scatterGraph);
+        console.log(this.selectGraph);
+        this.selectGraph.lineWidth = 2;
+        this.selectGraph.strokeColor.fromHEX('ee0000');
         this.drawAxis();
         this.drawScatter();
     }
 
     onEnable () {
         input.on(Input.EventType.KEY_DOWN, this.keyDown, this);
+        input.on(Input.EventType.KEY_PRESSING, this.keyPressing, this);
+        input.on(Input.EventType.KEY_UP, this.keyUp, this);
         input.on(Input.EventType.TOUCH_START, this.onTouchStart, this);
         input.on(Input.EventType.TOUCH_MOVE, this.onTouchMove, this);
         input.on(Input.EventType.TOUCH_END, this.onTouchEnd, this);  
@@ -110,58 +135,70 @@ export class test extends Component {
 
     onDisable () {
         input.off(Input.EventType.KEY_DOWN, this.keyDown, this);
+        input.off(Input.EventType.KEY_PRESSING, this.keyPressing, this);
+        input.off(Input.EventType.KEY_UP, this.keyUp, this);
         input.off(Input.EventType.TOUCH_START, this.onTouchStart, this);
         input.off(Input.EventType.TOUCH_MOVE, this.onTouchMove, this);
         input.off(Input.EventType.TOUCH_END, this.onTouchEnd, this);
     }
 
     update(deltaTime: number) {
-        
+        if (this.isSelect && this.isMove && !this.isSelectCtrl) {
+            this.selectGraph.clear();
+            // selectMovingPos没有做-20 -60校正处理
+            this.selectGraph.moveTo(this.selectPos.x + 20, this.selectPos.y + 60);
+            this.selectGraph.lineTo(this.selectMovingPos.x, this.selectPos.y + 60);
+            this.selectGraph.lineTo(this.selectMovingPos.x, this.selectMovingPos.y);
+            this.selectGraph.lineTo(this.selectPos.x + 20, this.selectMovingPos.y);
+            this.selectGraph.lineTo(this.selectPos.x + 20, this.selectPos.y + 60);
+            this.selectGraph.stroke();
+        }
     }
 
     private drawAxis() {
         // 原点(-320, 0)
-        this.graph.lineWidth = 3;
-        this.graph.strokeColor.fromHEX('#eeeeee');
+        // scattergraph的ui坐标被放在了屏幕中心，后面放到左下角可以统一0-1280/0-720坐标
+        this.scatterGraph.lineWidth = 3;
+        this.scatterGraph.strokeColor.fromHEX('#eeeeee');
         // x-axis
-        this.graph.moveTo(-620, 0);
-        this.graph.lineTo(-20, 0);
-        this.graph.lineTo(-30, 5);
-        this.graph.moveTo(-30, -5);
-        this.graph.lineTo(-20, 0);
+        this.scatterGraph.moveTo(-620, 0);
+        this.scatterGraph.lineTo(-20, 0);
+        this.scatterGraph.lineTo(-30, 5);
+        this.scatterGraph.moveTo(-30, -5);
+        this.scatterGraph.lineTo(-20, 0);
 
         // y-axis
-        this.graph.moveTo(-320, -300);
-        this.graph.lineTo(-320, 300);
-        this.graph.lineTo(-325, 290);
-        this.graph.moveTo(-320, 300);
-        this.graph.lineTo(-315, 290);
+        this.scatterGraph.moveTo(-320, -300);
+        this.scatterGraph.lineTo(-320, 300);
+        this.scatterGraph.lineTo(-325, 290);
+        this.scatterGraph.moveTo(-320, 300);
+        this.scatterGraph.lineTo(-315, 290);
 
         // origin-label
         const scaleLabel = new Node();
         const sl = scaleLabel.addComponent(Label);
-        this.graphics.addChild(scaleLabel);
+        this.ScatterGraphic.addChild(scaleLabel);
         sl.string = '0';
         sl.fontSize = 10;
         sl.lineHeight = sl.fontSize;
         sl.color.fromHEX('#eeeeee');
         scaleLabel.setPosition(new Vec3(-330, -10, 0));
-        scaleLabel.layer = this.graphics.layer;
+        scaleLabel.layer = this.ScatterGraphic.layer;
 
         const scaleLabelListX = [-560, -500, -440, -380, -260, -200, -140, -80];
         const scaleLabelListY = [-240, -180, -120, -60, 60, 120, 180, 240];
 
         for (let i = 0; i < 8; i++) {
-            this.graph.moveTo(scaleLabelListX[i], -5);
-            this.graph.lineTo(scaleLabelListX[i], 5);
-            this.graph.moveTo(-315, scaleLabelListY[i]);
-            this.graph.lineTo(-325, scaleLabelListY[i]);
+            this.scatterGraph.moveTo(scaleLabelListX[i], -5);
+            this.scatterGraph.lineTo(scaleLabelListX[i], 5);
+            this.scatterGraph.moveTo(-315, scaleLabelListY[i]);
+            this.scatterGraph.lineTo(-325, scaleLabelListY[i]);
             const scaleLabelX = new Node();
             const scaleLabelY = new Node();
             const slx = scaleLabelX.addComponent(Label);
             const sly = scaleLabelY.addComponent(Label);
-            this.graphics.addChild(scaleLabelX);
-            this.graphics.addChild(scaleLabelY);
+            this.ScatterGraphic.addChild(scaleLabelX);
+            this.ScatterGraphic.addChild(scaleLabelY);
             slx.string = lerp(this.scatterRange.left, this.scatterRange.right, (scaleLabelListX[i] + 620) / 600.0).toFixed(2).toString();   
 
             sly.string = lerp(this.scatterRange.bottom, this.scatterRange.top, (scaleLabelListY[i] + 300) / 600.0).toFixed(2).toString();   
@@ -173,16 +210,16 @@ export class test extends Component {
             sly.color.fromHEX('#eeeeee');
             scaleLabelX.setPosition(new Vec3(scaleLabelListX[i], -10, 0));
             scaleLabelY.setPosition(new Vec3(-335, scaleLabelListY[i], 0));
-            scaleLabelX.layer = this.graphics.layer;
-            scaleLabelY.layer = this.graphics.layer;
+            scaleLabelX.layer = this.ScatterGraphic.layer;
+            scaleLabelY.layer = this.ScatterGraphic.layer;
         }
 
         // this.graph.close();
-        this.graph.stroke();
+        this.scatterGraph.stroke();
     }
 
     private drawScatter() {
-        this.graph.lineWidth = 0;
+        this.scatterGraph.lineWidth = 0;
         this.scatterWidth = this.scatterRange.right - this.scatterRange.left;
         this.scatterHeight = this.scatterRange.top - this.scatterRange.bottom;
 
@@ -205,10 +242,10 @@ export class test extends Component {
             this.pointTree[Math.min(Math.floor(point.pos.x / 60), 9)][Math.min(Math.floor(point.pos.y / 60), 9)].push(point);
             // console.log(point.pos.x * 600 - 620, point.pos.y - 300);
             
-            this.graph.fillColor.fromHEX('#9999dd');
-            this.graph.circle(point.pos.x - 620, point.pos.y - 300, 2);
-            this.graph.fill();
-            this.graph.stroke();
+            this.scatterGraph.fillColor.fromHEX('#9999dd');
+            this.scatterGraph.circle(point.pos.x - 620, point.pos.y - 300, 2);
+            this.scatterGraph.fill();
+            this.scatterGraph.stroke();
         }
     }
 
@@ -221,12 +258,25 @@ export class test extends Component {
             this.isShow = !this.isShow;
             op.opacity = this.isShow ? 255 : 0;
         } else if (this.isShow) {
-            if (key.keyCode === KeyCode.KEY_A) {
-                // .....
+            console.log('is one? ' + this.selectType);
+            if (key.keyCode === KeyCode.CTRL_LEFT && !this.isSelect && this.selectType != SelectingType.One && this.selectType != SelectingType.Range) {
+                // 按住左ctrl多次选点
+                this.isSelectCtrl = true;
             }
         }
     }
 
+    private keyPressing(key: EventKeyboard) {
+        // if (key.keyCode === KeyCode.CTRL_LEFT) {
+        // }
+    }
+
+    private keyUp(key: EventKeyboard) {
+        if (key.keyCode === KeyCode.CTRL_LEFT) {
+            this.isSelectCtrl = false;
+        }
+    }
+ 
     private distanceVec2(v1: Vec2, v2: Vec2) {
       const n1: number = v2.x - v1.x;
       const n2: number = v2.y - v1.y;
@@ -243,11 +293,20 @@ export class test extends Component {
                 pos.subtract2f(20, 60);
                 this.selectPos = pos;
 
-                this.isSelectingOne = false;
-                this.isSelectingAny = false;
-                while (this.selectNodeList.length > 0) {
+                // 只要点击散点界面就取消所有选中状态
+                // this.isSelectingOne = false;
+                // this.isSelectingRange = false;
+                // if (!this.isSelectCtrl) {
+                //     this.isSelectingMulti = false;
+                // }
+                if (this.selectType != SelectingType.Multi || !this.isSelectCtrl) 
+                    this.selectType = SelectingType.None;
+                
+                // 这里也把数据点清空
+                while (!this.isSelectCtrl && this.selectNodeList.length > 0) {
                     this.selectNodeList[this.selectNodeList.length - 1].destroy();
                     this.selectNodeList.pop();
+
                 } 
             }
         }
@@ -260,7 +319,7 @@ export class test extends Component {
             console.log('moving');
             this.isMove = true;
             if (this.isSelect) {
-
+                this.selectMovingPos = pos;
             }
         } else {                        // 3d体素交互事件
             
@@ -268,10 +327,11 @@ export class test extends Component {
     }
 
     private onTouchEnd(e: EventTouch) {
+        console.log(this.isSelectCtrl);
         const pos: Vec2 = e.touch.getUILocation();
         if (this.isShow) {
             if (this.isSelect) {
-                if (this.isMove) {
+                if (this.isMove && !this.isSelectCtrl) {
                     pos.subtract2f(20, 60);
                     pos.x = Math.min(Math.max(0, pos.x), 600);
                     pos.y = Math.min(Math.max(0, pos.y), 600);
@@ -316,7 +376,10 @@ export class test extends Component {
                             }
                         }
                     }
-
+                    if (this.selectNodeList.length > 0) {
+                        this.selectType = SelectingType.Range;
+                        // TODO: 展示范围选择的button
+                    }
                 } else {
                     const tileX = Math.floor((pos.x - 20) / 60);
                     const tileY = Math.floor((pos.y - 60) / 60);
@@ -332,18 +395,31 @@ export class test extends Component {
                             console.log('shot on node!' + pointList[i].pos);
                             console.log(pos);
                             console.log(this.UICanvas.getChildByName('BackUI').children);
-                            this.isSelectingOne = true;
                             break;
                         }
                     }
+                    if (this.selectNodeList.length > 0) {
+                        if (!this.isSelectCtrl) {
+                            this.selectType = SelectingType.One;
+                            // TODO: 展示单选的button
+                        } else {
+                            this.selectType = SelectingType.Multi;
+                            // TODO: 展示多选的button
+                        }
+                    }
+                    // else
                 } 
-            } else if (pos.x > this.selectPanelPos.left && pos.x < this.selectPanelPos.right && pos.y > this.selectPanelPos.bottom && pos.y < this.selectPanelPos.top) {
-                console.log('in panel');
+            } else if (pos.x > this.quadPanelPos.left && pos.x < this.quadPanelPos.right && pos.y > this.quadPanelPos.bottom && pos.y < this.quadPanelPos.top) {
+
+                let uv: Vec2 = new Vec2((pos.x - this.quadPanelPos.left) / (this.quadPanelPos.right - this.quadPanelPos.left), 
+                    (pos.y - this.quadPanelPos.bottom) / (this.quadPanelPos.top - this.quadPanelPos.bottom));
+                console.log('panel uv:' + uv);
             }
         }
 
         this.isMove = false;
         this.isSelect = false;
+        this.selectGraph.clear();
     }
 }
 
